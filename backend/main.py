40c89c6603d7fe74ae18fe932c0c657a6ccde35c
robertemi <roeminovici@gmail.com>
2025-11-04 19:52:24 +0200

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,12 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
-from backend.rag.retriever import RAGService
-from backend.types.types import IndexProjectRequest, IndexTaskRequest
 from backend.api.routes import router as api_router
 
 
 app = FastAPI() 
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     # create mock project and task Data 
-#     rag = RAGService()
-
-#     rag.index_project(IndexProjectRequest(
-#         projectId=1,
-#         userId=101,
-#         name="AI Task Management Dashboard",
-#         description=(
-#             "A web-based platform that helps software teams automatically generate, "
-#             "enrich, and organize development tasks using AI. "
-#             "The system integrates with Jira and GitHub to streamline backlog refinement "
-#             "and estimate story points intelligently based on past work patterns."
-#         ),
-#         status="in_progress"
-#         )
-#     )
-
-#     rag.index_task(IndexTaskRequest(
-#         projectId=1,
-#         taskId=2,
-#         task_title='Login Screen',
-#         user_description='Create login screen'
-#     ))
-#     yield
-#     print(" App shutting down")
-
-# app.router.lifespan_context = lifespan
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(api_router)
 
